df3b6750884afd86df4b0e737ef85c02e9137377
sinagolakhe <125475781+sinagolakhe@users.noreply.github.com>
2024-08-13 04:07:02 +0330

    Update config.py

--- config.py
+++ config.py
@@ -4,42 +4,46 @@
 
 
 import os
+from dotenv import load_dotnev, dotnev_values
+load_dotenv()
 import logging
 from logging.handlers import RotatingFileHandler
 
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = "7264464627:AAFF8SXbtGsamZi7w7wTPVHQZSxKhbXjt1Y"
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-APP_ID = "28453201"
+APP_ID = int(os.environ.get("APP_ID", ""))
 
 #Your API Hash from my.telegram.org
-API_HASH = "2cc29365e02ff750682a9edd5d1ca14e"
+API_HASH = os.environ.get("API_HASH", "")
 
 #Your db channel Id
-CHANNEL_ID = "-1002184727848"
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 
 #OWNER ID
-OWNER_ID = "7433933634"
+OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 
 #Port
-PORT = "8080"
+PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = "mongodb+srv://shiroshare:shiroshare@golakh.nh074.mongodb.net/?retryWrites=true&w=majority&appName=golakh"
-DB_NAME = "golakh"
+DB_URI = os.environ.get("DATABASE_URL", "")
+DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = "0"
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
-TG_BOT_WORKERS = ""
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = "سلام {first}! با این بات می‌تونی به کار های شیروکامی دسترسی داشته باشی."
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
-    ADMINS="325065205"
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "").split()):
+        ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
